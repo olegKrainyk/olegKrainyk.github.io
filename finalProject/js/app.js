0cac176fa64ef7ea12a0871b1addbac2b62672a8
olegKrainyk <57600551+olegKrainyk@users.noreply.github.com>
2020-02-07 23:11:26 +0200

    nav with js

--- finalProject/js/app.js
+++ finalProject/js/app.js
@@ -19,6 +19,7 @@ window.onload = function () {
             body.classList.toggle('scroll-h');
         }
     });
+    navigation.render();
     footer.render();
 };
 
@@ -448,3 +449,26 @@ class Footer {
 }
 
 const footer = new Footer();
+
+
+
+
+//render nav for html pages
+class Nav {
+
+    render() {
+
+        const htmlNav = `
+        <ul>
+        <li><a href="newrel.html">New Releases</a></li>
+        <li><a href="men.html">Men</a></li>
+        <li><a href="women.html">Women</a></li>
+        </ul>
+        `;
+
+        document.querySelector('nav').innerHTML = htmlNav;
+    }
+
+}
+
+const navigation = new Nav();
