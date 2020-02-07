52dfbfa546173c95e7f430638b8b30e731c1a1be
olegKrainyk <57600551+olegKrainyk@users.noreply.github.com>
2020-02-07 23:32:37 +0200

    Update app.js

--- finalProject/js/app.js
+++ finalProject/js/app.js
@@ -23,7 +23,23 @@ window.onload = function () {
     footer.render();
 };
 
+//render nav for html pages
+class Nav {
 
+    render() {
+
+        const htmlNav = `
+        <ul>
+        <li><a href="newrel.html">New Releases</a></li>
+        <li><a href="men.html">Men</a></li>
+        <li><a href="women.html">Women</a></li>
+        </ul>
+        `;
+
+        document.querySelector('nav').innerHTML = htmlNav;
+    }
+}
+const navigation = new Nav();
 
 
 
@@ -47,7 +63,6 @@ class Products {
         }
         cartNum.render(products.length);
     }
-
 
 
     sortDef() {
@@ -449,26 +464,3 @@ class Footer {
 }
 
 const footer = new Footer();
-
-
-
-
-//render nav for html pages
-class Nav {
-
-    render() {
-
-        const htmlNav = `
-        <ul>
-        <li><a href="newrel.html">New Releases</a></li>
-        <li><a href="men.html">Men</a></li>
-        <li><a href="women.html">Women</a></li>
-        </ul>
-        `;
-
-        document.querySelector('nav').innerHTML = htmlNav;
-    }
-
-}
-
-const navigation = new Nav();
