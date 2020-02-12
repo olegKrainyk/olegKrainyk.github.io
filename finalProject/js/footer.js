de3ce3605a3f0de5550b2760414a40ec46ead079
olegKrainyk <57600551+olegKrainyk@users.noreply.github.com>
2020-02-12 05:36:50 +0200

    Update footer.js

--- finalProject/js/footer.js
+++ finalProject/js/footer.js
@@ -20,7 +20,7 @@ class Footer {
             <ul>
                 Contacts:
                 <li><a href="tel:+1-768-446-5533">Tel: +1 768 446 5533</a> </li>
-                <li><a href="https://www.instagram.com/whyiamthere/">Instagram</a></li>
+                <li><a href="https://www.instagram.com/whyiamthere/" target="blank">Instagram</a></li>
                 <li><a href="https://www.google.com.ua/maps/place/%D0%9B%D0%BE%D1%81-%D0%90%D0%BD%D0%B4%D0%B6%D0%B5%D0%BB%D0%B5%D1%81,+%D0%9A%D0%B0%D0%BB%D0%B8%D1%84%D0%BE%D1%80%D0%BD%D0%B8%D1%8F,+%D0%A1%D0%A8%D0%90/@34.0201613,-118.6919256,10z/data=!3m1!4b1!4m5!3m4!1s0x80c2c75ddc27da13:0xe22fdf6f254608f4!8m2!3d34.0526594!4d-118.2458496?hl=ru" target="blank">Location: LA, California, US</a></li>
                 <li><a href="mailto:ballersshoes@gmail.com" target="blank">E-mail: ballersshoes@gmail.com</a></li>
             </ul>
